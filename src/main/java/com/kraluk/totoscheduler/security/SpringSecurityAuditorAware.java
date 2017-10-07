7c6cfe49141f68f012611fee53ef6e2c9cf52feb
Lukasz Krauzowicz <kraluk@gmail.com>
2017-10-07 18:50:56 +0200

    Regenerated with JHipster 4.9.0

---
+++ src/main/java/com/kraluk/totoscheduler/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package com.kraluk.totoscheduler.security;
+
+import com.kraluk.totoscheduler.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return userName != null ? userName : Constants.SYSTEM_ACCOUNT;
+    }
+}
